26c38b528b356e2ab0ff810e6800a077627deb84
brunojclementino <brunojclementino@gmail.com>
2015-03-26 23:54:03 -0300

    Captura de imagem pela webcam
    Pessoal, é necessário que baixem o OpenCV
    Baixem a versão 2.4.10 em http://opencv.org/downloads.html
    Para configurar a biblioteca sigam esses passos:
    http://opencv.org/downloads.html
    
    OBS: Coloquem a pasta que for descompactada do OpenCV no disco C

---
+++ src/view/Cam.java
@@ -0,0 +1,44 @@
+package view;
+
+import org.opencv.core.Mat;
+import org.opencv.core.Size;
+import org.opencv.highgui.Highgui;
+import org.opencv.highgui.VideoCapture;
+import org.opencv.imgproc.Imgproc;
+
+public class Cam {
+	
+	public static void main(String[] args) {
+		System.out.println("Olá, OpenCV");
+		
+		System.loadLibrary("opencv_java2410");
+		
+		VideoCapture videoCapture = new VideoCapture(0);
+		
+		if(!videoCapture.isOpened()){
+			System.out.println("Não conectou sua câmera");
+		}
+		else{
+			System.out.println("WebCam funciona "+ videoCapture.toString());
+			
+		}
+		Mat frame = new Mat();
+		videoCapture.retrieve(frame);
+		
+		Highgui.imwrite("foto1.jpg", frame);
+		
+		Mat frameBlur = new Mat();
+		videoCapture.retrieve(frameBlur);
+		Highgui.imwrite("foto2.jpg", frameBlur);
+		
+		/*Imgproc.blur(frame, frameBlur, new Size(5,5));
+		Highgui.imwrite("foto2.jpg", frameBlur);
+		
+		Imgproc.GaussianBlur(frame, frameBlur, new Size(25,25), 20);
+		Highgui.imwrite("me3.jpg", frameBlur);*/
+		videoCapture.release();
+		
+		
+	}
+
+}
