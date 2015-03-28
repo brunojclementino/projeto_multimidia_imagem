5e0c7f156118e93cf470d764a0a8e41aeb094082
Lucas Miranda <lucasmirandadourado@gmail.com>
2015-03-27 22:34:23 -0300

    Atualizando
    Continua sem funcionar :(

--- src/view/GeraImagem.java
+++ src/view/GeraImagem.java
@@ -1,54 +1,54 @@
 package view;
 
+import javax.swing.JOptionPane;
+
 import org.opencv.core.Mat;
 import org.opencv.highgui.Highgui;
 
 public class GeraImagem {
 	
 	public GeraImagem(String caminhoImg1, String caminhoImg2) {
-		
+
 		System.loadLibrary("opencv_java2410");
-		
+
 		Mat frame1 = new Mat();
 		frame1 = Highgui.imread(caminhoImg1);
-		
+
 		Mat frame2 = new Mat();
 		frame2 = Highgui.imread(caminhoImg2);
 		
-		double [] t1 = new double[3];
-		double [] t2 = new double[3];
-		double [] t3 = new double[3];
-		System.out.println(t1[0]+"    "+t1[1]+"   "+t1[2]);
-		
+		double[] t1 = new double[3];
+		double[] t2 = new double[3];
+		double[] t3 = new double[3];
+		System.out.println(t1[0] + "    " + t1[1] + "   " + t1[2]);
+
 		int largura = (int) frame1.size().width;
 		int altura = (int) frame1.size().height;
-		
+
 		Mat frame3 = new Mat();
-		frame3=Highgui.imread(caminhoImg2);
-		
-		for(int i = 0;i<altura;i++){
-			for(int j=0;j<largura;j++){
-				
+		frame3 = Highgui.imread(caminhoImg2);
+
+		for (int i = 0; i < altura; i++) {
+			for (int j = 0; j < largura; j++) {
+
 				t1 = frame1.get(i, j);
 				t2 = frame2.get(i, j);
-				
-				
-				for(int k=0;k<3;k++){
+
+				for (int k = 0; k < 3; k++) {
 					t3[k] = Math.abs(t1[k] - t2[k]);
 				}
-				
+
 				frame3.put(i, j, t3);
-				//System.out.println(t3[0]+"    "+t3[1]+"   "+t3[2]);
+				// System.out.println(t3[0]+"    "+t3[1]+"   "+t3[2]);
 			}
 		}
-		
-		
-		
+
 		Highgui.imwrite("foto3.jpg", frame3);
+		JOptionPane.showMessageDialog(null, "Imagem gerada com sucesso!");
 	}
-	
-	public static void main(String[] args) {
-		new GeraImagem("fotos01\\foto1.jpg", "fotos02\\foto2.jpg");
-	}
+
+//	public static void main(String[] args) {
+//		new GeraImagem("fotos01\\foto1.jpg", "fotos02\\foto2.jpg");
+//	}
 
 }
--- src/view/TelaInicial.java
+++ src/view/TelaInicial.java
@@ -24,10 +24,7 @@ public class TelaInicial extends JFrame {
 	JComboBox comboBox_02 = new JComboBox();
 	JLabel panel_projecao01 = new JLabel();
 	JLabel panel_projecao02 = new JLabel();
-	
-	/**
-	 * Launch the application.
-	 */
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -41,20 +38,18 @@ public class TelaInicial extends JFrame {
 		});
 	}
 
-	/**
-	 * Create the frame.
-	 */
+	
 	public TelaInicial() {
 		setResizable(false);
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 1007, 454);
-		
+		setBounds(100, 100, 684, 454);
+
 		JMenuBar menuBar = new JMenuBar();
 		setJMenuBar(menuBar);
-		
+
 		JMenu mnFotos = new JMenu("Fotos");
 		menuBar.add(mnFotos);
-		
+
 		JMenuItem mntmTirarFotos = new JMenuItem("Tirar fotos");
 		mntmTirarFotos.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -64,22 +59,31 @@ public class TelaInicial extends JFrame {
 				comboBox_02.repaint();
 			}
 		});
-		mntmTirarFotos.setIcon(new ImageIcon(TelaInicial.class.getResource("/lib/Camera_icon.jpg")));
+		mntmTirarFotos.setIcon(new ImageIcon(TelaInicial.class
+				.getResource("/lib/Camera_icon.jpg")));
 		mnFotos.add(mntmTirarFotos);
-		
+
 		JMenuItem mntmAtualizar = new JMenuItem("Atualizar ");
 		mntmAtualizar.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
 				ImagemTela tela = new ImagemTela("fotos02\\foto2.jpg");
-				panel_projecao02.add(tela);
-				panel_projecao02.repaint();
 				
-				panel_projecao01.repaint();
+				panel_projecao02.add(tela);
+				
 			}
 		});
-		mntmAtualizar.setIcon(new ImageIcon(TelaInicial.class.getResource("/lib/atualizar.jpg")));
+		mntmAtualizar.setIcon(new ImageIcon(TelaInicial.class
+				.getResource("/lib/atualizar.jpg")));
 		mnFotos.add(mntmAtualizar);
+
+		JMenuItem mntmSubtraoDeFotos = new JMenuItem(
+				"Subtra\u00E7\u00E3o de fotos");
+		mntmSubtraoDeFotos.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				new GeraImagem("\fotos01\\foto01.jpg", "\fotos02\\foto02.jpg");
+			}
+		});
+		mnFotos.add(mntmSubtraoDeFotos);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -89,17 +93,17 @@ public class TelaInicial extends JFrame {
 		JPanel panel_foto01 = new JPanel();
 		panel_foto01.setBounds(5, 5, 320, 370);
 		contentPane.add(panel_foto01);
-		
+
 		comboBox_01.setLocation(10, 11);
 		comboBox_01.setSize(300, 31);
 		panel_foto01.setLayout(null);
 		panel_foto01.add(comboBox_01);
 		listaConteudo("fotos01", comboBox_01);
-		
+
 		panel_projecao01.setBackground(Color.GRAY);
 		panel_projecao01.setBounds(10, 53, 300, 300);
 		panel_foto01.add(panel_projecao01);
-		
+
 		// Panel que apresenta as fotos;
 		JPanel panel_foto02 = new JPanel();
 		panel_foto02.setBounds(335, 5, 329, 370);
@@ -108,22 +112,12 @@ public class TelaInicial extends JFrame {
 
 		comboBox_02.setBounds(10, 11, 300, 31);
 		panel_foto02.add(comboBox_02);
-		
+
 		listaConteudo("fotos02", comboBox_02);
-		
-		
+
 		panel_projecao02.setBackground(Color.GRAY);
 		panel_projecao02.setBounds(10, 53, 300, 300);
 		panel_foto02.add(panel_projecao02);
-		
-		JPanel panel = new JPanel();
-		panel.setLayout(null);
-		panel.setBounds(674, 5, 329, 370);
-		contentPane.add(panel);
-		
-		JLabel panel_1 = new JLabel();
-		panel_1.setBounds(10, 53, 300, 300);
-		panel.add(panel_1);
 
 		comboBox_01.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -132,7 +126,7 @@ public class TelaInicial extends JFrame {
 				panel_projecao01.repaint();
 			}
 		});
-		
+
 		comboBox_02.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				ImagemTela tela = new ImagemTela("fotos02\\foto2.jpg");
